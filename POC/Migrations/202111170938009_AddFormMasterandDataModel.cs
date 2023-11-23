194117e313c93c6cb8e16d5d6be24c7c3bc1efee
Maulik Dave <maulik.b.dave@stellanstechnosoft.com>
2023-11-23 10:52:26 +0530

    Initial Commit

---
+++ POC/Migrations/202111170938009_AddFormMasterandDataModel.cs
@@ -0,0 +1,41 @@
+﻿namespace POC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddFormMasterandDataModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.FormDataModels",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FormId = c.Int(nullable: false),
+                        Data = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.FormMasterModels", t => t.FormId, cascadeDelete: true)
+                .Index(t => t.FormId);
+            
+            CreateTable(
+                "dbo.FormMasterModels",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Form = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.FormDataModels", "FormId", "dbo.FormMasterModels");
+            DropIndex("dbo.FormDataModels", new[] { "FormId" });
+            DropTable("dbo.FormMasterModels");
+            DropTable("dbo.FormDataModels");
+        }
+    }
+}
